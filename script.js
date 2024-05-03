d725c0c92cb302d2c83a18a83b509f0e1e48878a
AllenHortilano <168819383+AllenHortilano@users.noreply.github.com>
2024-05-03 17:50:42 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+Let sections = document.querSelectorAll ('sections);
+Let navLinks = document.querSelectorAll('heaer nav a');
+
+window.onscroll = () => {
+sections.array.forEach(sec => {
+    Let top = windows.scrollY;
+});
+
+
+
+
+};
